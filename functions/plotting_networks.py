23465f456fb42457907ea1c7f1be9668e5050fe3
mbrochlips <m.brochlips@gmail.com>
2025-05-14 14:37:11 +0200

    update final

--- functions/plotting_networks.py
+++ functions/plotting_networks.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
+import ast
 import pandas as pd
 import networkx as nx
+from matplotlib.patches import Patch
 
 def basic_graph_stats(G):
     N = G.number_of_nodes()
@@ -56,4 +58,60 @@ def scatter_dist(G, F):
     plt.title("Degree Distribution Comparison (Log-Log)", fontsize=14, fontweight='bold')
     plt.legend()
     plt.grid(True, which="both", linestyle="--", linewidth=0.3)
+    plt.show()
+
+def plotNetwork_color(G):
+    #Genre hierarchy
+    hierarchy = ["eurovision pop", "jazz", "choral", "classical", "heavy metal", "country", "electronic", "folk" , "pop", "rock"]
+    # the hierarchy is defined in a way that the most common genres pop and rock are at the end of the list
+
+    def pick_genre(genres_raw):
+        if isinstance(genres_raw, str):
+            try:
+                genres = ast.literal_eval(genres_raw)
+            except (ValueError, SyntaxError):
+                genres = []
+        else:
+            genres = genres_raw or []
+
+        genres_lower = [g.lower().strip() for g in genres]
+
+        for genre in hierarchy:
+            if genre in genres_lower:
+                return genre
+        return "other"
+
+
+    genre_map = {node: pick_genre(G.nodes[node].get("genre")) for node in G.nodes()}
+
+
+    unique_genres = sorted(set(genre_map.values()))
+
+    color_map = {genre: plt.cm.tab10(i % 10) for i, genre in enumerate(unique_genres)}
+
+    #NOTE: is the right genre swapped??
+    color_map["other"], color_map["jazz"] = color_map["jazz"], color_map["other"]
+
+    node_colors = [color_map[genre_map[node]] for node in G.nodes()]
+
+
+    # Plot the graph
+    pos = nx.spring_layout(G, seed=42)
+    plt.figure(figsize=(12, 9))
+
+    nx.draw(
+        G,
+        pos,
+        node_color=node_colors,
+        with_labels=False,
+        node_size=50,
+        edge_color='gray'
+    )
+
+    # Create a legend as a table
+    legend_elements = [Patch(facecolor=color_map[genre], label=genre) for genre in unique_genres]
+    plt.legend(handles=legend_elements, title="Genre (Color)", loc="upper right", bbox_to_anchor=(1.15, 1.0))
+
+    plt.title("Network Colored by Representative Genre")
+    plt.tight_layout()
     plt.show()
